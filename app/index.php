ae648f539541b412ac54bd3bb883c70cd9d1ae64
Sajad Torkamani <sajadtorkamani1@gmail.com>
2023-10-13 22:06:48 +0100

    WIP

--- app/index.php
+++ app/index.php
@@ -1,7 +1,15 @@
 <?php
 
-$time = date("H:i:s");
+require_once './lib/database.php';
+
 ?>
 
-<p>Hello! I'm running inside a Docker container.</p>
-<p>The time now is <strong><?= $time ?></strong></p>
+<h1>Hello Docker!</h1>
+
+<p>Hi. I'm running inside a Docker container.</p>
+
+<p>I've successfully connected to a MySQL database that runs in a separate Docker container. How cool?</p>
+
+<img src="./img.png" alt="Docker" style="max-width: 100%; width: 400px">
+
+
