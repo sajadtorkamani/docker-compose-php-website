5a8deadf1350ef6ca5780fbffeab0df98bd56458
Sajad Torkamani <sajadtorkamani1@gmail.com>
2023-10-22 12:52:57 +0100

    WIP

---
+++ app/http/process-message.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once '../lib/database.php';
+
+$message = $_POST['message'] ?? '';
+
+$sql = 'INSERT INTO messages (body) VALUES (:body)';
+$stmt = $pdo->prepare($sql);
+$stmt->bindParam(':body', $message);
+$stmt->execute();
+
+header('Location: /');
+exit();
--- app/index.php
+++ app/index.php
@@ -2,14 +2,43 @@
 
 require_once './lib/database.php';
 
+$sql = 'SELECT * from messages';
+$stmt = $pdo->query($sql);
+
 ?>
 
 <h1>Hello Docker!</h1>
 
 <p>Hi. I'm running inside a Docker container.</p>
 
-<p>I've successfully connected to a MySQL database that runs in a separate Docker container. How cool?</p>
+<p>I've successfully connected to a MySQL database that runs in a separate
+  Docker container. How cool?</p>
 
 <img src="./img.png" alt="Docker" style="max-width: 100%; width: 400px">
+
+<p>
+  You persist data to MySQL using volumes and the data will be there even when
+  you stop and restart the container.
+</p>
+
+<section style="margin-bottom: 20px">
+  <h3>Messages (<?= $stmt->rowCount() ?>)</h3>
+    <?php while ($row = $stmt->fetch()): ?>
+      <article style="margin-bottom: 10px;">
+        <div style="margin-bottom: 5px;">
+            <?= $row['body'] ?>
+        </div>
+        <em>
+          (<?= $row['created_at'] ?>)
+        </em>
+      </article>
+      <hr>
+    <?php endwhile; ?>
+</section>
+
+<form action="http/process-message.php" method="post">
+  <input name="message" placeholder="Type a message" value="Hi" />
+  <button>Add message</button>
+</form>
 
 
